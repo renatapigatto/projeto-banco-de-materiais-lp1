e4a318ad190c2cb92b384bde8e7ad9af6b533abf
Renata Pigatto <renatapigatto@gmail.com>
2023-01-17 10:42:56 -0300

    logica de login

--- projeto-banco-de-materiais.c
+++ projeto-banco-de-materiais.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -13,21 +5,28 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <string.h>
 #include <unistd.h>
 
-
-int opMenu, exibirMenu, opInvalida;
+int opMenu, exibirMenu, opInvalida, estaLogado = 0;
 char limpaTela[5], pesquisa[100];
+char usuarios[10][2][20], nomeUsuario[20], senhaUsuario[6];
+
 
 void mostrarMenuInicial();
 void mostrarMenuPesquisa();
 void mostrarResultadoPesquisa();
 void mostrarCadastroMateriais();
+void scanPesquisa();
+void mostrarTelaLogin();
+void autenticarUsuario();
+void cadastrarUsuario();
+int verificarUsuarioExistente(void);
+void validarSenha();
 void mostrarMenuSair();
 
 void mostrarMenuInicial() {
     exibirMenu = 1;
     opInvalida = 0;
 
-    while (exibirMenu){
+    while(exibirMenu){
         printf("\n#########################################");
         printf("\n#   B A N C O  D E  M A T E R I A I S   #");
         printf("\n#########################################");
@@ -36,7 +35,6 @@ void mostrarMenuInicial() {
         printf("\n#   2-Cadastrar material                #");
         printf("\n#   3-Sair                              #");
         printf("\n#                                       #");
-
         if(!opInvalida){
              printf("\n#                                       #");
         }else{
@@ -44,7 +42,7 @@ void mostrarMenuInicial() {
         }
 
         printf("\n#########################################");
-        printf("\n\nDigite a opção desejada: ");
+        printf("\nDigite a opção desejada: ");
         scanf("%d", &opMenu);
         getchar();
 
@@ -56,7 +54,11 @@ void mostrarMenuInicial() {
                 break;
             case 2:
                 system(limpaTela);
-                mostrarCadastroMateriais();
+                if(estaLogado){
+                    mostrarCadastroMateriais();
+                }else{
+                    mostrarTelaLogin();
+                }
                 exibirMenu = 0;
                 break;
             case 3:
@@ -83,16 +85,7 @@ void mostrarMenuPesquisa() {
     printf("\n# Digite < para voltar                  #");
     printf("\n#########################################");
     printf("\nDigite sua busca: ");
-    scanf("%99s", pesquisa);
-    //limitar caracteres no scanf
-
-    if (pesquisa[0] == '<') {
-        system(limpaTela);
-        mostrarMenuInicial();
-    } else {
-        system(limpaTela);
-        mostrarResultadoPesquisa();
-    }
+    scanPesquisa();
 }
 
 void mostrarResultadoPesquisa(){
@@ -118,29 +111,219 @@ void mostrarResultadoPesquisa(){
     printf("\n - Como usar %s?           v", pesquisa);
     printf("\n---------------------------------------------");
     printf("\n\nDigite uma nova busca ou < para voltar para o menu inicial: ");
-    scanf("%99s", pesquisa);
+    scanPesquisa();
+}
+
+//refatorado para evitar repetição nas telas de pesquisa
+void scanPesquisa() {
+    strncpy(pesquisa, "", 100); //esvazia string pesquisa
     
+    scanf("%[^\n]", pesquisa);
+    getchar();
+
     if (pesquisa[0] == '<') {
         system(limpaTela);
         mostrarMenuInicial();
     } else {
         system(limpaTela);
-        mostrarResultadoPesquisa();
+        if (strlen(pesquisa) == 0) {
+            mostrarMenuPesquisa();
+        } else {
+            mostrarResultadoPesquisa();
+        }
     }
 }
 
 void mostrarCadastroMateriais(){
-    printf("Cadastro de materias indisponível no momento.");
-    printf("\n\nVocê será redirecionado para a página inicial...");
-    sleep(1.5);
-    printf("\n.");
-    sleep(1.5);
-    printf("\n.");
-    sleep(1.5);
-    printf("\n.");
-    sleep(1.5);
+    float t = 1.5; // add uma variável para facilitar futuramente caso mude o tempo de sleep()
+    
+    printf("\n\nCadastro de materias indisponível no momento.");
+    printf("\n\nVocê será redirecionado para a página inicial ");
+    sleep(t);
+    printf(". ");
+    sleep(t);
+    printf(". ");
+    sleep(t);
+    printf(".");
+    sleep(t);
     system(limpaTela);
     mostrarMenuInicial();
+}
+
+void mostrarTelaLogin(){
+    
+    exibirMenu = 1;
+    opInvalida = 0;
+    
+    while(exibirMenu){
+        printf("\n#########################################");
+        printf("\n#   B A N C O  D E  M A T E R I A I S   #");
+        printf("\n#########################################");
+        printf("\n#                                       #");
+        printf("\n#   1-Logar-se                          #");
+        printf("\n#   2-Cadastrar-se                      #");
+        printf("\n#                                       #");
+        if(!opInvalida){
+            printf("\n#  Digite 0 para voltar                 #");
+        }else{
+            printf("\n#            Opção inválida!            #");
+        }
+        printf("\n#########################################");
+        printf("\nDigite a opção desejada: ");
+        scanf("%d", &opMenu);
+        getchar();
+        
+        switch(opMenu){
+                case 1:
+                    system(limpaTela);
+                    exibirMenu = 0;
+                    autenticarUsuario();
+                    break;
+                case 2:
+                    system(limpaTela);
+                    exibirMenu = 0;
+                    cadastrarUsuario();
+                    break;
+                case 0:
+                    system(limpaTela);
+                    exibirMenu = 0;
+                    mostrarMenuInicial();
+                    break;
+                default:
+                    system(limpaTela);
+                    opInvalida = 1;
+        }
+    }
+    
+}
+
+void cadastrarUsuario(){
+    
+    float t = 1.5;
+    int i, usuarioValido;
+    
+    strcpy(usuarios[0][0], "renata");
+    
+    exibirMenu = 1;
+    
+    while(exibirMenu){
+        usuarioValido = verificarUsuarioExistente();
+        
+        if(usuarioValido){
+            exibirMenu = 0;
+        }else{
+            system(limpaTela);
+            printf("\nUsuário existente! Tente outro nome de usuário.");
+        }
+    }
+    
+    do{
+        printf("\nDigite uma senha:\n> ");
+        scanf("%s", senhaUsuario);
+        getchar();
+    }while(senhaUsuario == "");
+    
+    for(i = 0; i < 10; i++){
+        if(strcmp(usuarios[i][0], "") == 0){
+            strcpy(usuarios[i][0], nomeUsuario);
+            strcpy(usuarios[i][1], senhaUsuario);
+            break;
+        }
+    }
+    
+    system(limpaTela);
+    
+    printf("\nUsuário cadastrado com sucesso!");
+    printf("\n---------------------------------------------");
+    printf("\nRedirecionando para a página de login ");
+    sleep(t);
+    printf(". ");
+    sleep(t);
+    printf(". ");
+    sleep(t);
+    printf(".");
+    sleep(t);
+    system(limpaTela);
+    mostrarTelaLogin();
+    
+}
+
+int verificarUsuarioExistente(void){
+   
+    int i, usuarioValido = 1;
+    
+    do{
+        printf("\nDigite um nome de usuário\n> ");
+        scanf("%s", nomeUsuario);
+        getchar();
+        if(nomeUsuario == ""){
+            system(limpaTela);
+        }
+    }while(nomeUsuario == "");
+    
+    for(i = 0; i < 10; i++){
+        if(strcmp(nomeUsuario, usuarios[i][0]) == 0){
+            usuarioValido = 0;
+            break;
+        }
+    }
+    
+    return(usuarioValido);
+}
+
+void autenticarUsuario(){
+    
+    int i, opcao = 0, usuarioRegistrado = 0;
+    
+    strcpy(usuarios[0][0], "renata");
+    
+    printf("\nUSUÁRIO: ");
+    scanf("%s", nomeUsuario);
+    getchar();
+    printf("SENHA: ");
+    scanf("%s", senhaUsuario);
+    getchar();
+    
+    for(i = 0; i < 10; i++){
+        if(strcmp(usuarios[i][0], nomeUsuario) == 0){
+            if(strcmp(usuarios[i][1], senhaUsuario) == 0){
+                sleep(1);
+                printf("\n\nSeja bem-vinde, %s!", nomeUsuario);
+                system(limpaTela);
+                estaLogado = 1;
+                usuarioRegistrado = 1;
+                mostrarCadastroMateriais();
+            }else{
+                sleep(1);
+                printf("\n\nSenha incorreta! Digite seus dados novamente.");
+                system(limpaTela);
+                autenticarUsuario();
+            }
+        }
+    }
+    
+    if(!usuarioRegistrado){
+        sleep(1);
+        printf("\n\nUsuário não existe!");
+        printf("\n\n---------------------------------------------");
+        printf("\n1-Fazer nova tentativa de Login");
+        printf("\n2-Fazer o cadastro");
+        while((opcao != 1) || (opcao != 2)){
+            printf("\n\nQual a opção desejada? ");
+            scanf("%d", &opcao);
+            if(opcao == 1){
+                system(limpaTela);
+                autenticarUsuario();
+            }else{
+                if(opcao == 2){
+                    system(limpaTela);
+                    cadastrarUsuario();
+                }else{
+                    printf("\nOpção inválida!");
+                }
+            }
+        }
+    }
 }
 
 void mostrarMenuSair() {
@@ -195,51 +378,10 @@ int main()
         setlocale(LC_ALL, "Portuguese");
 	#else
 	    strncpy(limpaTela, "clear", 5);
+        setlocale(LC_ALL, "pt_BR.UTF-8");
 	#endif
 
     mostrarMenuInicial();
 
     return 0;
 }
-/*
-
-    //////////////////////////////////////////////////////////
-    ////////////////////BLOCO DE AUTENTICACAO/////////////////
-    //////////////////////////////////////////////////////////
-
-    //procedimento lerDadosUsuario
-
-    int i, usuarioRejeitado = 1;
-    char usuario[100], senha[6];
-
-    //usuarios[10,0] <- "gabrielhamdan"
-    //usuarios[10,1] <- "123"
-
-    printf("USU�RIO: ");
-    scanf("%s", usuario);
-    printf("SENHA: ");
-    scanf("%s", senha);
-
-    for(i = 0; i<=10; i++){
-        //SE usuario = usuarios[i,0] ENTAO
-            //SE senha = usuarios[i,1] ENTAO
-                //printf(clear)
-                //usuarioRejeitado = 0
-                //autenticarUsuario
-    }
-
-    if (usuarioRejeitado == 1){
-        //rejeitarUsuario;
-    }
-
-
-    //procedimento autenticarUsuario
-    printf("Usu�rio autenticado!");
-
-
-    //procedimento rejeitarUsuario
-    printf("Usu�rio ou senha inv�lido!");
-
-}
-
-*/
